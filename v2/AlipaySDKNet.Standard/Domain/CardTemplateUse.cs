7adaeb60cd5289c7be39abeac8652921a99bdf5e
姚烽 <yaofeng@fengwall.com>
2024-12-24 17:12:07 +0800

    1

--- v2/AlipaySDKNet.Standard/Domain/CardTemplateUse.cs
+++ v2/AlipaySDKNet.Standard/Domain/CardTemplateUse.cs
@@ -42,12 +42,6 @@ namespace Aop.Api.Domain
         public string ReservationUrl { get; set; }
 
         /// <summary>
-        /// 周期卡是否全部门店
-        /// </summary>
-        [XmlElement("show_shop")]
-        public string ShowShop { get; set; }
-
-        /// <summary>
         /// 可用次数或期数
         /// </summary>
         [XmlElement("usable_count")]
